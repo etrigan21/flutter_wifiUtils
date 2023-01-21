195e4f00918d4fe49553b1ef5fee0df899b15533
EDZEL AGULTO <esagulto@up.edu.ph>
2023-01-21 15:00:24 +0800

    added native methods

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 # flutter_wifi_utils
 
 This is a personal test project as a way for me to learn the development of flutter plugins. This project utilizes the ThanosFisherman's WifiUtils to provide the user controls over an Android phone's WiFi. 
-
+This plugin is particularly useful when dealing with IoT projects. 
 
--- android/src/main/java/fun/etrigan/flutter_wifi_utils/WiFi/WiFiCommands.java
+++ android/src/main/java/fun/etrigan/flutter_wifi_utils/WiFi/WiFiCommands.java
@@ -1,14 +1,117 @@
 package fun.etrigan.flutter_wifi_utils.WiFi;
 
 import android.content.Context;
+import android.net.wifi.ScanResult;
+import android.os.Build;
+import android.util.Log;
+
+import androidx.annotation.NonNull;
 
 import com.thanosfisherman.wifiutils.WifiUtils;
-import Flutter
+import com.thanosfisherman.wifiutils.wifiConnect.ConnectionErrorCode;
+import com.thanosfisherman.wifiutils.wifiConnect.ConnectionSuccessListener;
+import com.thanosfisherman.wifiutils.wifiRemove.RemoveErrorCode;
+import com.thanosfisherman.wifiutils.wifiRemove.RemoveSuccessListener;
+import com.thanosfisherman.wifiutils.wifiWps.ConnectionWpsListener;
+
+import java.lang.reflect.Method;
+import java.util.List;
+
+import io.flutter.plugin.common.MethodChannel;
+
+//import Flutter
 public class WiFiCommands {
+    Boolean isScanAvailable = false;
+    List<ScanResult> oldScanResults;
     public void enableWiFi(Context context, MethodChannel.Result result){
         WifiUtils.withContext(context).enableWifi((boolean success)->{
-            if(success){
-            } else {}
+            result.success(success);
+        });
+    }
+
+    public void disableWiFi(Context context){
+        WifiUtils.withContext(context).disableWifi();
+    }
+
+    public Boolean getIsScanAvailable(){
+        return isScanAvailable;
+    }
+    public void scanWiFi(Context context, MethodChannel.Result result){
+        WifiUtils.withContext(context).scanWifi(
+                (@NonNull List<ScanResult> scanRes) -> {
+                    if(scanRes.isEmpty()){
+                        Log.d("New", "false");
+
+                    } else {
+                        Log.d("New", "true");
+                    }
+                    List<ScanResult> values = scanRes;
+                    for (ScanResult s : values){
+                        Log.d("item", s.SSID);
+                    }
+                }
+        ).start();
+    }
+
+    public void connectToWiFi(Context context, MethodChannel.Result result, String SSID, String Password, int timeout){
+        WifiUtils.withContext(context)
+                .connectWith(SSID, Password)
+                .setTimeout(timeout)
+                .onConnectionResult(new ConnectionSuccessListener() {
+                    @Override
+                    public void success() {
+                        result.success(true);
+                    }
+
+                    @Override
+                    public void failed(@NonNull ConnectionErrorCode errorCode) {
+                        result.success(false);
+                    }
+                })
+                .start();
+    }
+
+    public void connectToWiFiViaSSIDAndBSSID(Context context, MethodChannel.Result result,String SSID, String BSSID, String password, int timeout){
+        WifiUtils.withContext(context)
+                .connectWith(SSID, BSSID, password).setTimeout(timeout)
+                .onConnectionResult(new ConnectionSuccessListener() {
+                    @Override
+                    public void success() {
+                        result.success(true);
+                    }
+
+                    @Override
+                    public void failed(@NonNull ConnectionErrorCode errorCode) {
+                        result.success(false);
+                    }
+                })
+                .start();
+    }
+
+    public void connectWithWPS(Context context , String BSSID, String password, MethodChannel.Result result){
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+            WifiUtils.withContext(context).connectWithWps(BSSID, password).onConnectionWpsResult(new ConnectionWpsListener() {
+                @Override
+                public void isSuccessful(boolean isSuccess) {
+                    result.success(isSuccess);
+                }
+            }).start();
+        } else {
+            result.success(false);
+        }
+    }
+
+    public void disconnectAndRemove(Context context, String SSID, MethodChannel.Result result){
+        WifiUtils.withContext(context).remove(SSID, new RemoveSuccessListener() {
+            @Override
+            public void success() {
+                result.success(true);
+            }
+
+            @Override
+            public void failed(@NonNull RemoveErrorCode errorCode) {
+                result.success(false);
+            }
         });
     }
 }
